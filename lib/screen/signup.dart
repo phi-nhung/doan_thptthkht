268bedaae3344a32fc10ed5537b5778fb38bdf78
unknown <noreply@github.com>
2025-06-05 12:57:12 +0700

    trang dang ky

--- lib/screen/signup.dart
+++ lib/screen/signup.dart
@@ -1,61 +1,75 @@
-import 'package:doan/screen/signup.dart';
+import 'package:doan/screen/login.dart';
 import 'package:flutter/material.dart';
-import 'user_home.dart'; // Import trang UserHome
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class Signup extends StatelessWidget {
+  const Signup({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        //title: const Text('Login'),
-      ),
+      
       body: Center(
         child: SingleChildScrollView(
           child: Container(
             margin: const EdgeInsets.all(20),
-            child: Column(          
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const CircleAvatar(
                   radius: 50,
                   backgroundImage: AssetImage('assets/images/logo.png'),
                   backgroundColor: Colors.transparent,
-                 // Transparent background
                 ),
+                const SizedBox(height: 20),
                 const Text(
                   'Xin chào!',
                   style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.blueAccent),
                 ),
                 const SizedBox(height: 20),
                 const Text(
-                  'Vui lòng đăng nhập để tiếp tục',
+                  'Vui lòng nhập thông tin đăng ký',
                   style: TextStyle(fontSize: 18, color: Colors.grey),
                 ),
                 const SizedBox(height: 20),
-                 TextField(
+                TextField(
                   decoration: InputDecoration(
-                    
-                    labelText: 'Số CCCD/CMND',
-                    labelStyle: TextStyle(fontSize: 20),
+                    labelText: 'Họ và tên',
                     prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                    border: OutlineInputBorder(),
                   ),
-                  
                 ),
                 const SizedBox(height: 10),
-                 TextField(
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Số điện thoại',
+                    prefixIcon: Icon(Icons.phone),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Mã số CCCD/CMND',
+                    prefixIcon: Icon(Icons.credit_card),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
                   obscureText: true,
                   decoration: InputDecoration(
                     labelText: 'Mật khẩu',
-                    labelStyle: TextStyle(fontSize: 20),
                     prefixIcon: Icon(Icons.lock),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Nhập lại mật khẩu',
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
                   ),
                 ),
                 const SizedBox(height: 20),
@@ -64,7 +78,7 @@ class Login extends StatelessWidget {
                     // Đăng nhập thành công, chuyển sang trang user_home
                     Navigator.pushReplacement(
                       context,
-                      MaterialPageRoute(builder: (context) => UserHome()),
+                      MaterialPageRoute(builder: (context) => Login()),
                     );
                   },
                   style: ElevatedButton.styleFrom(
@@ -74,27 +88,8 @@ class Login extends StatelessWidget {
                     ),
                     backgroundColor: Colors.blue, // Button color
                   ), 
-                  child: Text('Đăng nhập', style: TextStyle(fontSize: 20, color: Colors.white)),
+                  child: Text('Đăng ký', style: TextStyle(fontSize: 20, color: Colors.white)),
                 ),
-                const SizedBox(height: 10),
-                TextButton(
-                  onPressed: (){}, 
-                  child:Text('Quên mật khẩu?', style: TextStyle(color: Colors .brown, fontSize: 20),)
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Bạn chưa có tài khoản?', style: TextStyle(fontSize: 20)),
-                    
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => Signup()));
-                      }, 
-                      child:Text('Đăng ký?', style: TextStyle(color: Colors.red, fontSize: 20),)
-                    ),
-                  ],
-                )
               ],
             ),
           ),
